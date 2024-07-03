05399a9af7850e89d1cbd4dbe87745227760fea4
Joe Loera <joeloera7@gmail.com>
2024-07-02 22:36:42 -0700

    Navbar functional and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 const App = () => {
   return (
     <div
-      className="bg-cover bg-center"
-      style={{
-        backgroundImage: "./assets/makeupbrushes.jpg",
-      }}
+    // className="bg-cover bg-center"
+    // style={{
+    //   backgroundImage: "url(./src/assets/makeupbrushes.jpg)",
+    // }}
     >
       <Navbar />
-      {/* Additional content will go here */}
+      <div className="max-w-7xl mx-auto pt-20 px-">
+        <HeroSection />
+      </div>
     </div>
   );
 };
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,19 @@
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h3 className="text-3xl sm:text-5xl lg:text-6xl text-center tracking-wide font-semibold text-black leading-normal">
+        “Beauty begins the moment you decide to be yourself”
+        <br />- Charlotte Tilbury
+      </h3>
+      <p className="mt-10 sm:mt-20 text-xl sm:text-2xl lg:text-3xl text-center text-neutral-500 max-w-xs sm:max-w-lg lg:max-w-4xl">
+        Michelle, a top makeup artist in Los Angeles, excels in creating
+        stunning looks for any occasion. With years of experience and a talent
+        for enhancing natural beauty, her personalized services leave clients
+        looking and feeling their best. For weddings, events, or photoshoots,
+        Michelle is the go-to artist in LA.
+      </p>
+    </div>
+  );
+};
+
+export default HeroSection;
