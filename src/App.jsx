f7c51737c7d1da1a7b24c62143d21d146bc34858
Joe Loera <joeloera7@gmail.com>
2024-07-10 22:13:45 -0700

    Created and imported footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import ImageCarousel from "./components/ImageCarousel";
 import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <HeroSection />
 
       <Services />
+      <Footer />
     </>
   );
 };
